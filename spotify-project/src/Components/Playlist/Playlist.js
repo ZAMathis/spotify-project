c13a4ab34ded6afb41c000dd4f7098e70243419e
Zachary Mathis <zachamathis@gmail.com>
2022-11-02 13:23:26 -0700

    added starting files, created starting components

---
+++ spotify-project/src/Components/Playlist/Playlist.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+import React from 'react';
+import { TrackList } from 'spotify-project\src\Components\TrackList\TrackList.js';
+import './Playlist.css';
+
+export class Playlist extends React.Component {
+    render() {
+        return (
+            <div className="Playlist">
+                <input defaultValue={'New Playlist'}/>
+                <TrackList />
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        );
+    }
+}
