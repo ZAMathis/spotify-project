6a9d4f4bf29bfd0ab3135e7e427c8090a5c8893f
Zachary Mathis <zachamathis@gmail.com>
2022-11-02 15:27:47 -0700

    changed name of variable in map method

--- spotify-project/src/Components/TrackList/TrackList.js
+++ spotify-project/src/Components/TrackList/TrackList.js
@@ -6,11 +6,11 @@ export class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                this.props.tracks.map(track => {
+                this.props.tracks.map( currentTrack => {
                     // eslint-disable-next-line no-undef
                     <Track 
-                    track={track} 
-                    key={track.id} 
+                    track={currentTrack} 
+                    key={currentTrack.id} 
                     onAdd={this.props.onAdd} 
                     onRemove={this.props.onRemove}
                     isRemoval={this.props.isRemoval} />
